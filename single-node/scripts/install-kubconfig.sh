d7ce31ab0668d85774dfbd238af70e39bff7a601
Michael Mitchell <Michael Mitchell>
2016-05-27 16:49:48 +0700

    folder syncing config and fix bugs

--- single-node/scripts/install-kubconfig.sh
+++ single-node/scripts/install-kubconfig.sh
@@ -6,9 +6,9 @@ kubectl config set-cluster vagrant-single-cluster \
   --certificate-authority=$DIR/../ssl/ca.pem
 
 kubectl config set-credentials vagrant-single-admin \
-  --certificate-authority==$DIR/../ssl/ca.pem \
-  --client-key==$DIR/../ssl/admin-key.pem \
-  --client-certificate==$DIR/../ssl/admin.pem
+  --certificate-authority=$DIR/../ssl/ca.pem \
+  --client-key=$DIR/../ssl/admin-key.pem \
+  --client-certificate=$DIR/../ssl/admin.pem
 
 kubectl config set-context vagrant-single \
   --cluster=vagrant-single-cluster \

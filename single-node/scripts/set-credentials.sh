ddb6610303307411d56000e84d2649d9fff4e066
Michael Mitchell <michael@michaelmitchell.co.nz>
2016-11-02 12:14:16 +0200

    scripts & tweaks
    adds helper scripts and makes changes to support local docker registry

---
+++ single-node/scripts/set-credentials.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+kubectl config set-cluster vagrant-single-cluster \
+  --server=https://172.18.0.101:8443 \
+  --certificate-authority=$DIR/../ssl/ca.pem
+
+kubectl config set-credentials vagrant-single-admin \
+  --certificate-authority=$DIR/../ssl/ca.pem \
+  --client-key=$DIR/../ssl/admin-key.pem \
+  --client-certificate=$DIR/../ssl/admin.pem
+
+kubectl config set-context vagrant-single \
+  --cluster=vagrant-single-cluster \
+  --user=vagrant-single-admin
+
+kubectl config use-context vagrant-single
